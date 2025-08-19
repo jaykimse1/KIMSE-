3ee87b6cff3012351d36c87956812d87512f42be
jaykimse1 <jaidenkisua@gmail.com>
2025-08-19 18:04:57 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~Nr0HiJDY#Mq9UKYNeJoqbzTrjHzv8vOQR9K4-iFrHWldOhli2nT4",  //
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "254115556779", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
